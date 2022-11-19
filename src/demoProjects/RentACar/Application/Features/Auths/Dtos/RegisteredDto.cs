dde64de54393b53ed9d747dbf81dcb004bc3c76a
Isa Civan <civanisaa@gmail.com>
2022-11-19 17:47:53 +0300

    Add Jwt Authentication

---
+++ src/demoProjects/RentACar/Application/Features/Auths/Dtos/RegisteredDto.cs
@@ -0,0 +1,8 @@
+﻿
+namespace Application.Features.Auths.Dtos
+{
+    public class RegisteredDto : RefreshedTokenDto 
+    {
+        //
+    }
+}
---
+++ src/demoProjects/RentACar/WebApi/Controllers/AuthController.cs
@@ -0,0 +1,34 @@
+﻿using Application.Features.Auths.Commands.Register;
+using Application.Features.Auths.Dtos;
+using Core.Security.Dtos;
+using Core.Security.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : BaseController
+    {
+        [HttpPost("Register")]
+        public async Task<IActionResult> Register([FromBody] UserForRegisterDto userForRegisterDto)
+        {
+            RegisterCommand registerCommand = new()
+            {
+                UserForRegisterDto = userForRegisterDto,
+                IpAdress = GetIpAddress()
+            };
+
+            RegisteredDto result = await Mediator.Send(registerCommand);
+            SetrefreshTokenToCookie(result.RefreshToken);
+            return Created("", result.AccessToken);
+        }
+
+        private void SetrefreshTokenToCookie(RefreshToken refreshToken)
+        {
+            CookieOptions cookieOptions = new() { HttpOnly = true, Expires = DateTime.Now.AddDays(7) };
+            Response.Cookies.Append("refreshToken",refreshToken.Token,cookieOptions);
+        }
+    }
+}
